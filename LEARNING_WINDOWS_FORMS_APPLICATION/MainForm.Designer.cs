827e640561d9043d1ea037c488b586eb096055c6
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2017-11-16 16:45:32 +0330

    Add project files.

---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	partial class MainForm
+	{
+		private System.ComponentModel.IContainer components = null;
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.insertButton = new System.Windows.Forms.Button();
+			this.dataTextBox = new System.Windows.Forms.TextBox();
+			this.dataListBox = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// insertButton
+			// 
+			this.insertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.insertButton.Location = new System.Drawing.Point(252, 12);
+			this.insertButton.Name = "insertButton";
+			this.insertButton.Size = new System.Drawing.Size(45, 23);
+			this.insertButton.TabIndex = 1;
+			this.insertButton.Text = "...";
+			this.insertButton.UseVisualStyleBackColor = true;
+			this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+			// 
+			// dataTextBox
+			// 
+			this.dataTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataTextBox.Location = new System.Drawing.Point(12, 12);
+			this.dataTextBox.Name = "dataTextBox";
+			this.dataTextBox.Size = new System.Drawing.Size(234, 23);
+			this.dataTextBox.TabIndex = 0;
+			// 
+			// dataListBox
+			// 
+			this.dataListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataListBox.FormattingEnabled = true;
+			this.dataListBox.ItemHeight = 16;
+			this.dataListBox.Location = new System.Drawing.Point(12, 41);
+			this.dataListBox.Name = "dataListBox";
+			this.dataListBox.Size = new System.Drawing.Size(285, 132);
+			this.dataListBox.TabIndex = 2;
+			this.dataListBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataListBox_KeyDown);
+			// 
+			// MainForm
+			// 
+			this.AcceptButton = this.insertButton;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(309, 186);
+			this.Controls.Add(this.dataListBox);
+			this.Controls.Add(this.dataTextBox);
+			this.Controls.Add(this.insertButton);
+			this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.MinimumSize = new System.Drawing.Size(325, 225);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Main";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button insertButton;
+		private System.Windows.Forms.TextBox dataTextBox;
+		private System.Windows.Forms.ListBox dataListBox;
+	}
+}
---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
@@ -0,0 +1,327 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, System.EventArgs e)
+		{
+		}
+
+		private void insertButton_Click(object sender, System.EventArgs e)
+		{
+			// **********
+			//dataListBox.Items.Add(dataTextBox.Text);
+			// **********
+
+			// **********
+			//dataTextBox.Text = dataTextBox.Text.Trim();
+
+			//if (dataTextBox.Text != "")
+			//{
+			//	dataListBox.Items.Add(dataTextBox.Text);
+			//}
+			// **********
+
+			// **********
+			//dataTextBox.Text = dataTextBox.Text.Trim();
+
+			//if (dataTextBox.Text != string.Empty)
+			//{
+			//	dataListBox.Items.Add(dataTextBox.Text);
+			//}
+			// **********
+
+			// **********
+			// "\n\r" -> System.Environment.NewLine
+			// **********
+
+			// **********
+			//dataTextBox.Text = dataTextBox.Text.Trim();
+
+			//if (dataTextBox.Text != string.Empty)
+			//{
+			//	if (!dataListBox.Items.Contains(dataTextBox.Text))
+			//		dataListBox.Items.Add(dataTextBox.Text);
+			//}
+			// **********
+
+			// **********
+			//dataTextBox.Text = dataTextBox.Text.Trim();
+
+			//if (dataTextBox.Text != string.Empty)
+			//{
+			//	if (dataListBox.Items.Contains(dataTextBox.Text) == false)
+			//	{
+			//		dataListBox.Items.Add(dataTextBox.Text);
+			//	}
+			//}
+			// **********
+
+			// **********
+			//dataTextBox.Text = dataTextBox.Text.Trim();
+
+			//dataTextBox.Text = dataTextBox.Text.Replace("  ", " ");
+			//dataTextBox.Text = dataTextBox.Text.Replace("   ", " ");
+			//dataTextBox.Text = dataTextBox.Text.Replace("    ", " ");
+			//dataTextBox.Text = dataTextBox.Text.Replace("     ", " ");
+
+			//if (dataTextBox.Text != string.Empty)
+			//{
+			//	if (dataListBox.Items.Contains(dataTextBox.Text) == false)
+			//	{
+			//		dataListBox.Items.Add(dataTextBox.Text);
+			//	}
+			//}
+			// **********
+
+			// **********
+			//dataTextBox.Text =
+			//	dataTextBox.Text.Trim();
+
+			//while (dataTextBox.Text.Contains("  "))
+			//{
+			//	dataTextBox.Text =
+			//		dataTextBox.Text.Replace("  ", " ");
+			//}
+
+			//if (dataTextBox.Text != string.Empty)
+			//{
+			//	if (dataListBox.Items.Contains(dataTextBox.Text) == false)
+			//	{
+			//		dataListBox.Items.Add(dataTextBox.Text);
+			//	}
+			//}
+			// **********
+
+			// **********
+			//dataTextBox.Text =
+			//	dataTextBox.Text.Trim();
+
+			//while (dataTextBox.Text.Contains("  "))
+			//{
+			//	dataTextBox.Text =
+			//		dataTextBox.Text.Replace("  ", " ");
+			//}
+
+			//if (dataTextBox.Text != string.Empty)
+			//{
+			//	if (dataListBox.Items.Contains(dataTextBox.Text) == false)
+			//	{
+			//		dataListBox.Items.Add(dataTextBox.Text);
+			//	}
+
+			//	dataTextBox.Text = string.Empty;
+			//}
+			// **********
+
+			// **********
+			//dataTextBox.Text =
+			//	dataTextBox.Text.Trim();
+
+			//while (dataTextBox.Text.Contains("  "))
+			//{
+			//	dataTextBox.Text =
+			//		dataTextBox.Text.Replace("  ", " ");
+			//}
+
+			//if (dataTextBox.Text != string.Empty)
+			//{
+			//	if (dataListBox.Items.Contains(dataTextBox.Text) == false)
+			//	{
+			//		dataListBox.Items.Add(dataTextBox.Text);
+			//	}
+
+			//	dataTextBox.SelectAll();
+			//}
+			// **********
+
+			// **********
+			// **********
+			// **********
+			//dataTextBox.Text =
+			//	dataTextBox.Text.Trim();
+
+			//while (dataTextBox.Text.Contains("  "))
+			//{
+			//	dataTextBox.Text =
+			//		dataTextBox.Text.Replace("  ", " ");
+			//}
+
+			//if (dataTextBox.Text != string.Empty)
+			//{
+			//	if (dataListBox.Items.Contains(dataTextBox.Text) == false)
+			//	{
+			//		dataListBox.Items.Add(dataTextBox.Text);
+			//	}
+
+			//	dataTextBox.Focus();
+
+			//	// **********
+			//	//dataTextBox.SelectAll();
+			//	// **********
+
+			//	// **********
+			//	//dataTextBox.Select
+			//	//	(start: 0, length: dataTextBox.Text.Length);
+			//	// **********
+
+			//	// **********
+			//	dataTextBox.SelectionStart = 0;
+			//	dataTextBox.SelectionLength = dataTextBox.Text.Length;
+			//	// **********
+			//}
+			// **********
+			// **********
+			// **********
+
+			// **********
+			dataTextBox.Text =
+				dataTextBox.Text.Trim();
+
+			while (dataTextBox.Text.Contains("  "))
+			{
+				dataTextBox.Text =
+					dataTextBox.Text.Replace("  ", " ");
+			}
+
+			if (dataTextBox.Text != string.Empty)
+			{
+				if (dataTextBox.Text.Length == 1)
+				{
+					dataTextBox.Text =
+						dataTextBox.Text.ToUpper();
+				}
+				else
+				{
+					dataTextBox.Text =
+						dataTextBox.Text.Substring(startIndex: 0, length: 1).ToUpper() +
+						dataTextBox.Text.Substring(startIndex: 1).ToLower();
+				}
+
+				if (dataListBox.Items.Contains(dataTextBox.Text) == false)
+				{
+					dataListBox.Items.Add(dataTextBox.Text);
+				}
+
+				dataTextBox.Focus();
+				dataTextBox.SelectAll();
+			}
+			// **********
+		}
+
+		private void dataListBox_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+		{
+			// **********
+			//if (e.KeyCode == System.Windows.Forms.Keys.Delete)
+			//{
+			//}
+			// **********
+
+			// **********
+			//switch (e.KeyCode)
+			//{
+			//	case System.Windows.Forms.Keys.Delete:
+			//	{
+			//		break;
+			//	}
+			//}
+			// **********
+
+			// **********
+			//switch (e.KeyCode)
+			//{
+			//	case System.Windows.Forms.Keys.Delete:
+			//	{
+			//		if ((e.Alt == false) &&
+			//			(e.Shift == false) &&
+			//			(e.Control == false))
+			//		{
+			//			dataListBox.Items.RemoveAt(dataListBox.SelectedIndex);
+			//		}
+
+			//		break;
+			//	}
+			//}
+			// **********
+
+			// **********
+			//switch (e.KeyCode)
+			//{
+			//	case System.Windows.Forms.Keys.Delete:
+			//	{
+			//		if ((e.Alt == false) &&
+			//			(e.Shift == false) &&
+			//			(e.Control == false))
+			//		{
+			//			if (dataListBox.SelectedIndex >= 0)
+			//			{
+			//				dataListBox.Items.RemoveAt(dataListBox.SelectedIndex);
+			//			}
+			//		}
+
+			//		break;
+			//	}
+			//}
+			// **********
+
+			// **********
+			//switch (e.KeyCode)
+			//{
+			//	case System.Windows.Forms.Keys.Delete:
+			//	{
+			//		if ((e.Alt == false) &&
+			//			(e.Shift == false) &&
+			//			(e.Control == false))
+			//		{
+			//			if (dataListBox.SelectedIndex >= 0)
+			//			{
+			//				int selectedIndex =
+			//					dataListBox.SelectedIndex;
+
+			//				dataListBox.Items.RemoveAt(selectedIndex);
+			//				dataListBox.SelectedIndex = selectedIndex;
+			//			}
+			//		}
+
+			//		break;
+			//	}
+			//}
+			// **********
+
+			// **********
+			switch (e.KeyCode)
+			{
+				case System.Windows.Forms.Keys.Delete:
+				{
+					if ((e.Alt == false) &&
+						(e.Shift == false) &&
+						(e.Control == false))
+					{
+						if (dataListBox.SelectedIndex >= 0)
+						{
+							int selectedIndex =
+								dataListBox.SelectedIndex;
+
+							dataListBox.Items.RemoveAt(selectedIndex);
+
+							if (selectedIndex > dataListBox.Items.Count - 1)
+							{
+								selectedIndex =
+									dataListBox.Items.Count - 1;
+							}
+
+							dataListBox.SelectedIndex = selectedIndex;
+						}
+					}
+
+					break;
+				}
+			}
+			// **********
+		}
+	}
+}
